0c7ac83417496c1d3c2a62bbc5e58351f1c920ef
shbodake <sayalibodake28@gmail.com>
2021-11-17 13:51:03 +0530

    Added: Created a regex pattern for validating user Password

--- src/com/blp/regexuser/RegexUserMain.java
+++ src/com/blp/regexuser/RegexUserMain.java
@@ -12,6 +12,7 @@ public class RegexUserMain {
                     + "Enter 2 To Check Second Name\n"
                     + "Enter 3 To Check EmailId\n"
                     + "Enter 4 To Check Mobile Number\n"
+                    + "Enter 5 To Check Password\n"
                     + "Enter 0 To Exit");
             int choice = scanner.nextInt();
             switch (choice) {
